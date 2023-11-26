8caead8da9ebe0eb985ecc8f533f0155f0036b11
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-11-26 20:09:20 +0600

    all issued fixed i guess

--- api/index.js
+++ api/index.js
@@ -23,14 +23,14 @@ mongoose
 
   const __dirname = path.resolve();
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
-app.use(cookieParser())
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000!!!");
-});
+  app.use(express.json());
+  app.use(cookieParser())
+  
+  app.listen(5000, () => {
+    console.log("Server is running on port 5000!!!");
+  });
 
 
 app.use("/api/user", userRouter)
